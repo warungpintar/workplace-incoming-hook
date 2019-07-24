0a11970d7b2adbdb26cc054fce774ca222947f6a
Muhamad Enrinal <muhamadenrinal@gmail.com>
2019-07-24 16:26:47 +0700

    add comment

--- helper/groupdetection.go
+++ helper/groupdetection.go
@@ -17,8 +17,13 @@ func GroupDetection(url string) string {
 
 		This format using this url format "git@gitlab.warungpintar.co:back-end/gitlab-test.git"
 
+		Flow :
+		1. Split git@gitlab.warungpintar.co:back-end/gitlab-test.git to [ back-end/gitlab-test.git]
+		2. Split [ back-end/gitlab-test.git] to [back-end gitlab-test.git]
+		3. Get string in slice[0] which is backend
+
 	*/
-	link := strings.Split(url, "git@gitlab.warungpintar.co:")
+	link := strings.Split(url, "git@gitlab.warungpintar.co:") //change git@gitlab.warungpintar.co: tou your own url
 	link = strings.Split(link[1], "/")
 	group := link[0]
 	return group
