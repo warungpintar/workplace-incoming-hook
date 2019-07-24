4bd6be47c628b60711a344cd9477160f7d6cfec1
Muhamad Enrinal <muhamadenrinal@gmail.com>
2019-07-24 16:14:19 +0700

    add some comment in code

--- helper/groupdetection.go
+++ helper/groupdetection.go
@@ -4,7 +4,20 @@ import (
 	"strings"
 )
 
+/*
+	GroupDetection method is to get group info from url
+
+	@param url
+
+*/
 func GroupDetection(url string) string {
+	/*
+		Split URL base on gitlab.warungpintar.co url
+		You can change with your own url
+
+		This format using this url format "git@gitlab.warungpintar.co:back-end/gitlab-test.git"
+
+	*/
 	link := strings.Split(url, "git@gitlab.warungpintar.co:")
 	link = strings.Split(link[1], "/")
 	group := link[0]
